d1eae9e6acd168f9efdeaa4f2d6dd2baff9b41e0
luis-dkouros <luis.dublatex@gmail.com>
2026-03-07 13:31:12 +0000

    Force rebuild v2

--- src/imopro.jsx
+++ src/imopro.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL; // 
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
