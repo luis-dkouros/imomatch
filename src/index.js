11f1c38fa7a357dd60f6cfb0941d37222a5effef
luis-dkouros <luis.dublatex@gmail.com>
2026-03-07 09:33:53 +0000

    ImoPro inicial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ImoPro from './imopro';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ImoPro />);
